e35d854a7055028d6ca18d1ea38824d065614786
Maxime FRERE <maxime.frere@timeonegroup.com>
2026-09-14 12:04:09 +0200

    chore(release): 2.7.0 🚀

--- dist/isdk.js
+++ dist/isdk.js
@@ -1,5 +1,5 @@
 
-/*! isdk 2.6.3 https://github.com/TimeOne-Group/isdk#readme @license GPL-3.0 */
+/*! isdk 2.7.0 https://github.com/TimeOne-Group/isdk#readme @license GPL-3.0 */
 (function () {
 	'use strict';
 
@@ -5921,7 +5921,7 @@
 	      value: null
 	    });
 	    this.env = "production";
-	    this.version = "2.6.3";
+	    this.version = "2.7.0";
 	    _classPrivateMethodGet(this, _setProgids, _setProgids2).call(this);
 	    _classPrivateMethodGet(this, _setCookieDomain, _setCookieDomain2).call(this);
 	    _classPrivateMethodGet(this, _configureProgramData, _configureProgramData2).call(this, CONSTANTS.cashback);
